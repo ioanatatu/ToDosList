c87f8f62c760036a58fd041733737c6a2b073b04
pndbear <37593571+pndbear@users.noreply.github.com>
2019-06-27 14:40:38 +0200

    JS

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-console.log('connected');
+// 00_GENERALL
+// 00.01_LATERREMOVE lines are there to check code and should be removed later
+console.log('js connected');
 
+function printLocation() {
+    console.trace();
+}
 
+// 01_GLOBAL VARIABLES ----------------------------------------------------------------------------------------------------------------
+let nameList = document.querySelector('.site-title');
+// 01.01_NUMBERS ==========
+
+// 02_EVENT LISTENERS ------------------------------------------------------------------------------------
+
+// 03_FUNCTIONS -----------------------------------------------------------------
+// 03.01_OPEN/ CLOSE SLIDING DRAWER and change icon ----------------------------------------------------------- 
 $('#closeBtn').click(function () {
     console.log('connected');
     $('.main-navigation').toggleClass('open');
@@ -13,93 +26,79 @@ $('#closeBtn').click(function () {
         btn.attr('class', 'fas fa-chevron-circle-down');
     }
 });
-
-// 3.4.DELETES TODO WHEN CLICK ON X ------------------------------------------------------------------------------------------------------------------------
-$("ul").on("click", "span", function(event){
-    $(this).parent().fadeOut(500, function(){
-        $(this).remove();
-    });
-    event.stopPropagation();
-});
-// 3.3.STRIKES THROUGH IF AN ITEM IS COMPLETED ------------------------------------------------------------------------------------------------------------------------
-$("ul[class='list']").on("click", "li", function(){
-    $(this).toggleClass("completed");
-});
-// 3.2.ADDS INPUT - CREATES NEW LI - TO ONE OF THE LISTS ------------------------------------------------------------------------------------------------------------------------
-$(".submit").on("click", function() {
-    var input = $("input[type='text']").val();
-    // checks if input equals the string "enter an item", otherwise it will add "enter an item" as item to the list
-    if(input !== "enter an item") {
-        // clears the content that we just enetered
-        $("input[type='text']").val("");
-        // checks if the value is not an empty string
-        if(input !== "" && input !== " ") {
-            // create a new li and add to ul
-            console.log("clicked");
-            $("ul[class='list']").append("<li><span><i class='fas fa-trash-alt'></i></span>" + input + "</li>");
-        } else {
-            $("input[type='text']").val("enter an item");
-            $("input[type='text']").on("click", function() {
-                $(this).val("");
-            });
-        }
-    }
-});
-
-
-// 03.02_EDIT THE NAMES OF PLAYERS ----------------------------------------------------------
+// 03.02_EDIT LIST NAME AND PASS IT TO THE TITLE ON THE SLIDING DRAWER ----------------------------------------------------------
 document.querySelector('.container').addEventListener('click', event => {
     const listName = document.querySelector('.site-title'); // targeted element
     let clickTarget = event.target; // clicked element
-    
+    let listNameOnSlide = document.querySelector('.title span');
+    console.log(listName.textContent);
+
     if (clickTarget === listName) {
-        document.querySelector('.site-title').contentEditable = 'true';
-        document.querySelector('.site-title').classList.add('editable');
-        
-        document.querySelector('.site-title').addEventListener('keydown', removeEditableWithEnterKey);
+        nameList.contentEditable = 'true';
+        nameList.classList.add('editable');
+
+        nameList.addEventListener('keydown', removeEditableWithEnterKey);
     } else {
-        document.getElementById('playerNameDisplay1').innerHTML = document.getElementById('playerNameSrc1').textContent;
-        console.log(document.getElementById('playerNameDisplay1').innerHTML);
-        console.log('clicked away'); // LATERREMOVE
-        document.querySelector('.player-1').classList.remove('editable');
-        document.querySelector('.player-1').addEventListener('keydown', removeEditableWithEnterKey);
+        replaceListName(listNameOnSlide);
+        nameList.classList.remove('editable');
+        nameList.addEventListener('keydown', removeEditableWithEnterKey);
     }
 });
 // 03.02_ASSOCIATED FUNCTIONS
 // 03.02.01_REMOVE EDITABILITY WITH ENTER KEY
 function removeEditableWithEnterKey() {
-    if(event.keyCode === 13) {
-        document.querySelector('.site-title').contentEditable = 'false';
-        document.querySelector('.site-title').classList.toggle('editable');
-        document.getElementById('playerNameDisplay1').innerHTML = document.getElementById('playerNameSrc1').textContent;
-        console.log(document.getElementById('playerNameDisplay1').innerHTML + 'from inside the function');   // LATERREMOVE
+    let listNameOnSlide = document.querySelector('.title span');
+
+    if (event.keyCode === 13) {
+        nameList.contentEditable = 'false';
+        nameList.classList.toggle('editable');
+        nameList.classList.remove('editable');
+        replaceListName(listNameOnSlide);
     }
+}
+
+function replaceListName(listNameOnSlide) {
+    listNameOnSlide.textContent = nameList.textContent;
+    listNameOnSlide.style.fontWeight = '600';
 }
 
 
 
+// testing *********************************************************
 
 
 
 
 
 
-//let navButton = document.querySelector('#closeBtn');
-//
-//navButton.addEventListener('click', function () {
-//    let nav = document.querySelector('nav');
-//    let btn = document.querySelector('#closeBtn');
-//    console.log(nav.hasAttribute('class', 'open'));
-//    
-//    if (nav.hasAttribute('class', 'open')) {
-//        console.log(nav.hasAttribute('class', 'main-navigation open'));
-//        console.log('its closed');
-//        nav.classList.remove('open');
-//        btn.setAttribute('class', 'fas fa-chevron-circle-up');
-//    } else {
-//        console.log('its open');
-//        nav.classList.add('open');
-//        btn.setAttribute('class', 'fas fa-chevron-circle-up');
-//        console.log(nav.classList);
-//    }
-//});
+// 3.4.DELETES TODO WHEN CLICK ON X ------------------------------------------------------------------------------------------------------------------------
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+// 3.3.STRIKES THROUGH IF AN ITEM IS COMPLETED ------------------------------------------------------------------------------------------------------------------------
+$("ul[class='list']").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+// 3.2.ADDS INPUT - CREATES NEW LI - TO ONE OF THE LISTS ------------------------------------------------------------------------------------------------------------------------
+$(".submit").on("click", function () {
+    var input = $("input[type='text']").val();
+    // checks if input equals the string "enter an item", otherwise it will add "enter an item" as item to the list
+    if (input !== "enter an item") {
+        // clears the content that we just enetered
+        $("input[type='text']").val("");
+        // checks if the value is not an empty string
+        if (input !== "" && input !== " ") {
+            // create a new li and add to ul
+            console.log("clicked");
+            $("ul[class='list']").append("<li><span><i class='fas fa-trash-alt'></i></span>" + input + "</li>");
+        } else {
+            $("input[type='text']").val("enter an item");
+            $("input[type='text']").on("click", function () {
+                $(this).val("");
+            });
+        }
+    }
+});
